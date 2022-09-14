cecd97d97140523c07f1b6aea6fa5f19e7a66ae6
hiroyasu tawara <zen_qp_mai@icloud.com>
2022-09-14 18:31:28 +0900

    お問い合わせ内容表示機能 実装

--- main.js
+++ main.js
@@ -6,4 +6,22 @@ $(function() {
     let index = $('.tab li').index($(this));
     $('.content li').eq(index).removeClass('hide');
   });
+
+  $('#checkButton').on('click', function(){
+    msgq.innerText = questionTextarea.value;
+    msgr.innerText = requestTextarea.value;
+    msgf.innerText = freeTextarea.value;
+  });
+
+  let questionTextarea = document.getElementById('question');
+  let requestTextarea = document.getElementById('request');
+  let freeTextarea = document.getElementById('free');
+  questionTextarea.value = '質問';
+  requestTextarea.value = '要望';
+  freeTextarea.value = '自由記入欄';
+
+  let msgq = document.getElementById('msg question');
+  let msgr = document.getElementById('msg request');
+  let msgf = document.getElementById('msg free');
+  
 });
